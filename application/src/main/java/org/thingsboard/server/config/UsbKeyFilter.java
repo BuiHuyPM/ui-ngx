c2dc6d2116299d943d802d98cd6523b532f9e462
hungtn <hungtn@amitech.vn>
2023-04-14 17:57:44 +0700

    add filter USB license

--- application/src/main/java/org/thingsboard/server/config/UsbKeyFilter.java
+++ application/src/main/java/org/thingsboard/server/config/UsbKeyFilter.java
@@ -1,25 +1,30 @@
 package org.thingsboard.server.config;
 
 import Key.KeyObj;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.Integer.parseInt;
+import java.util.HashMap;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class UsbKeyFilter implements Filter {
     @Value("${licenseKey}")
     private Integer licenseKey;
+
+    @Autowired
+    private Configuration freemarkerConfig;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -32,28 +37,33 @@ public class UsbKeyFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
         String uri =  req.getRequestURI();
 
-        List<String> allowedUris = new ArrayList<>();
-        allowedUris.add("/api/v1/\\w+/telemetry");
-        allowedUris.add("/api/v1/\\w+/attributes");
-        allowedUris.add("/api/v1/\\w+/attributes/updates");
-        allowedUris.add("/api/v1/\\w+/rpc");
-        allowedUris.add("/api/v1/\\w+/rpc/\\w+");
-        allowedUris.add("/api/v1/\\w+/claim");
-        allowedUris.add("/api/v1/\\w+/provision");
-        allowedUris.add("/api/v1/\\w+/firmware");
+//        List<String> allowedUris = new ArrayList<>();
+//        allowedUris.add("/api/v1/.*");
+//        allowedUris.add("/api/v1/\\w+/telemetry");
+//        allowedUris.add("/api/v1/\\w+/attributes");
+//        allowedUris.add("/api/v1/\\w+/attributes/updates");
+//        allowedUris.add("/api/v1/\\w+/rpc");
+//        allowedUris.add("/api/v1/\\w+/rpc/\\w+");
+//        allowedUris.add("/api/v1/\\w+/claim");
+//        allowedUris.add("/api/v1/\\w+/provision");
+//        allowedUris.add("/api/v1/\\w+/firmware");
+//        boolean isMatch = allowedUris.stream().anyMatch(uri::matches);
 
-        boolean isMatch = allowedUris.stream().anyMatch(uri::matches);
-
-        if (!isMatch){
-            KeyObj mUniKey = new KeyObj();
+        if (!uri.matches("/api/v1/.*")){
+            KeyObj keyObj = new KeyObj();
             short[] handle = new short[1];
             int[] lp1 = new int[1];
             int[] lp2 = new int[2];
-            long result = mUniKey.UniKey_Find(handle, lp1, lp2);
-            if (result != mUniKey.SUCCESS || licenseKey != lp1[0]) {
+            long result = keyObj.UniKey_Find(handle, lp1, lp2);
+            if (result != keyObj.SUCCESS || licenseKey != lp1[0]) {
                 res.setHeader("Content-Type", "charset=UTF-16LE");
                 res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                res.getWriter().write("Please insert your license USB key into the server's USB port !");
+                Template template = freemarkerConfig.getTemplate("no_license.ftl");
+                try {
+                    res.getWriter().write( FreeMarkerTemplateUtils.processTemplateIntoString(template, new HashMap<String,String>()));
+                } catch (TemplateException e) {
+                    res.getWriter().write("Please insert your license USB key into the server's USB port !");
+                }
                 res.getWriter().flush();
                 return;
             }
