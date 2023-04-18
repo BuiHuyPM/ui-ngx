7ef530dfe7cd029dcfc8241f751105b30554da10
hungtn <hungtn@amitech.vn>
2023-04-18 11:28:03 +0700

    update UUID

--- application/src/main/java/org/thingsboard/server/config/UsbKeyFilter.java
+++ application/src/main/java/org/thingsboard/server/config/UsbKeyFilter.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.thingsboard.server.utils.HardwareUtils;
@@ -20,10 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -34,7 +30,6 @@ public class UsbKeyFilter implements Filter {
     @Autowired
     private Configuration freemarkerConfig;
 
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -44,14 +39,14 @@ public class UsbKeyFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        String uri =  req.getRequestURI();
+        String uri = req.getRequestURI();
         List<String> allowedUris = new ArrayList<>();
         allowedUris.add("/api/v1/.*");
         allowedUris.add("/static/.*");
         allowedUris.add("/assets/.*");
         allowedUris.add(".*\\/\\w*(.json|.ico|.css|.js|.png|.svg|.jpg|.ttf)");
         boolean isMatch = allowedUris.stream().anyMatch(uri::matches);
-        if (!isMatch && !"$2a$10$vsTE270ueipXz227XeTDnuzcDVPrQqSfe4AoRGQdtlIVPJStGYbBu".equals(licenseKey)){
+        if (!isMatch && !"xxxxxxxx-xxxx-Bxxx-Axxx-xxxxxxxxxxxx".equals(licenseKey)) {
             KeyObj keyObj = new KeyObj();
             short[] handle = new short[1];
             int[] lp1 = new int[1];
@@ -59,32 +54,36 @@ public class UsbKeyFilter implements Filter {
             long result = keyObj.UniKey_Find(handle, lp1, lp2);
             String hacAddress = HardwareUtils.getMACAddress();
             String licenseUsb = String.valueOf(lp1[0]);
-            String prefixKey = "hungtn";
-            if (result != keyObj.SUCCESS || !passwordEncoder.matches(prefixKey+hacAddress+licenseUsb, licenseKey )) {
-                    res.setStatus(HttpServletResponse.SC_PAYMENT_REQUIRED);
-                    String contentType = req.getContentType();
-                    if (!Objects.equals(contentType, "application/json")){
-                        try {
-                            Template template = freemarkerConfig.getTemplate("no_license.ftl");
-                            response.setContentType("text/html");
-                            res.getWriter().write( FreeMarkerTemplateUtils.processTemplateIntoString(template, new HashMap<String,String>()));
-                            res.getWriter().flush();
-                            return;
-                        } catch (TemplateException ignored) {
+            String prefixKey = "h-u-n-g-d-z-a-i";
 
-                        }
+            String key = prefixKey + hacAddress + licenseUsb;
+            String uuid = UUID.nameUUIDFromBytes(key.getBytes()).toString();
+
+            if (result != keyObj.SUCCESS || !uuid.equals(licenseKey)) {
+                res.setStatus(HttpServletResponse.SC_PAYMENT_REQUIRED);
+                String contentType = req.getContentType();
+                if (!Objects.equals(contentType, "application/json")) {
+                    try {
+                        Template template = freemarkerConfig.getTemplate("no_license.ftl");
+                        response.setContentType("text/html");
+                        res.getWriter().write(FreeMarkerTemplateUtils.processTemplateIntoString(template, new HashMap<String, String>()));
+                        res.getWriter().flush();
+                        return;
+                    } catch (TemplateException ignored) {
+
                     }
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                    response.setContentType("application/json");
-                    HashMap<String, Object> body = new HashMap<>();
-                    body.put("message","Please insert your license USB key into the server's USB port!");
-                    body.put("status",HttpServletResponse.SC_PAYMENT_REQUIRED);
-                    body.put("timestamp", LocalDateTime.now().format(formatter));
-                    Gson gson = new Gson();
-                    res.getWriter().write(gson.toJson(body));
-                    res.getWriter().flush();
-                    return;
                 }
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                response.setContentType("application/json");
+                HashMap<String, Object> body = new HashMap<>();
+                body.put("message", "Please insert your license USB key into the server's USB port!");
+                body.put("status", HttpServletResponse.SC_PAYMENT_REQUIRED);
+                body.put("timestamp", LocalDateTime.now().format(formatter));
+                Gson gson = new Gson();
+                res.getWriter().write(gson.toJson(body));
+                res.getWriter().flush();
+                return;
+            }
         }
         chain.doFilter(request, response);
     }
