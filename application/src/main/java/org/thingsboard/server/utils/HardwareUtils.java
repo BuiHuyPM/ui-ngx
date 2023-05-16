1dd79dc5cdb4b667dfe5d8f21161318628bebd75
hungtn <hungtn@amitech.vn>
2023-05-16 18:00:32 +0700

    update macAddress

--- application/src/main/java/org/thingsboard/server/utils/HardwareUtils.java
+++ application/src/main/java/org/thingsboard/server/utils/HardwareUtils.java
@@ -5,19 +5,30 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class HardwareUtils {
-    public static String getMACAddress() throws UnknownHostException, SocketException {
-        InetAddress localHost = InetAddress.getLocalHost();
-        NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
-        byte[] hardwareAddress = ni.getHardwareAddress();
-        if (hardwareAddress == null){
-            return null;
+    public static String getMACAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        String macAddress = null;
+        while(networkInterfaces.hasMoreElements())
+        {
+            NetworkInterface network = networkInterfaces.nextElement();
+            byte[] mac = network.getHardwareAddress();
+            if(mac == null)
+            {
+                System.out.println("null mac");
+            }
+            else
+            {
+                String[] hexadecimal = new String[mac.length];
+                for (int i = 0; i < mac.length; i++) {
+                    hexadecimal[i] = String.format("%02X", mac[i]);
+                }
+                macAddress = String.join("-", hexadecimal);
+                break;
+            }
         }
-        String[] hexadecimal = new String[hardwareAddress.length];
-        for (int i = 0; i < hardwareAddress.length; i++) {
-            hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
-        }
-        return String.join("-", hexadecimal);
+        return macAddress;
     }
 }
