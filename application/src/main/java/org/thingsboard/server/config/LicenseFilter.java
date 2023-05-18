ebfb11c83c2c83220b2fd9188da9a753e83a0bcb
hungtn <hungtn@amitech.vn>
2023-05-18 08:04:32 +0700

    update filter

--- application/src/main/java/org/thingsboard/server/config/LicenseFilter.java
+++ application/src/main/java/org/thingsboard/server/config/LicenseFilter.java
@@ -2,7 +2,6 @@ package org.thingsboard.server.config;
 
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
