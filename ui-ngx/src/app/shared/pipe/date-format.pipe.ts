e68fc03a7aa76201a450bfc54cdd21a12e76afce
hungtn <hungtn@amitech.vn>
2023-06-14 18:54:58 +0700

    update folder

--- ui-ngx/src/app/shared/pipe/date-format.pipe.ts
+++ ui-ngx/src/app/shared/pipe/date-format.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
 
   transform(value: string): string {
+    if (!value){
+      return '';
+    }
     try {
       return new Date(value).toLocaleDateString();
     }catch (e) {
